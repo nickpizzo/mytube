0568a131ddb5f3814d80cd701daef6a027a101ee
Nick Pizzo <nick.pizzo@gmail.com>
2019-07-08 15:41:21 -0700

    video preview card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import HeaderNav from "./containers/HeaderNav/HeaderNav";
 import { SideBar } from "./containers/SideBar/SideBar";
+import { Home } from "./containers/Home/Home";
+// import "./Home.scss";
 
 function App() {
   return (
     <React.Fragment>
       <HeaderNav />
       <SideBar />
+      <Home />
     </React.Fragment>
   );
 }
