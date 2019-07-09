4f3aed4549f06859c3d53f9391190845ca42c48a
Nick Pizzo <nick.pizzo@gmail.com>
2019-07-09 15:27:09 -0700

    video grid components

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
 import "./Home.scss";
+import { VideoGrid } from "../../components/VideoGrid/VideoGrid";
 
 export class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <VideoPreview />
+        <VideoGrid title="Trending" />
+        <VideoGrid title="Autos & Vehicles" hideDivider={true} />
       </div>
     );
   }
